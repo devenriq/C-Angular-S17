6e240097cea75332cbc0bf8ea32b3b6018059f2c
enrique palomino <octepc.97@gmail.com>
2023-01-12 12:00:43 -0500

    made the basic structure

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NosotrosComponent,
+    ContactoComponent
+  ],
+  exports:[
+    HomeComponent,
+    NosotrosComponent,
+    ContactoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
